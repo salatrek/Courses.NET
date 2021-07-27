255fdcd9bd4575b0671d46b4777a70cf1721f6ed
AmberWarder <alexei.270396@gmail.com>
2021-07-27 20:33:44 +0300

    Added CreateFigure method

--- Lessons.NET/OOPLessons/Program.cs
+++ Lessons.NET/OOPLessons/Program.cs
@@ -13,78 +13,16 @@ namespace OOPLessons
             var figuresCountingPerimeter = new List<IPerimeterCalculation>();
             var listOfFigures = new List<Figure>();
 
-            try
-            {
-                figuresCountingPerimeter.Add(new Circle(5));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            CreateFigure(figuresCountingPerimeter, new Circle(5));
+            CreateFigure(figuresCountingPerimeter, new RightTriangle(3, 4, 5));
 
-            try
-            {
-                figuresCountingPerimeter.Add(new RightTriangle(3, 4, 5));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            try
-            {
-                listOfFigures.Add(new Quadrate(5));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            try
-            {
-                listOfFigures.Add(new Circle(5));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            try
-            {
-                listOfFigures.Add(new Triangle(5, 9, 12));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            try
-            {
-                listOfFigures.Add(new EquilateralTriangle(6, 6, 6));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            try
-            {
-                listOfFigures.Add(new IsoscelesTriangle(5, 6, 6));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            try
-            {
-                listOfFigures.Add(new RightTriangle(3, 4, 5));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
+            CreateFigure(listOfFigures, new Quadrate(5));
+            CreateFigure(listOfFigures, new Triangle(5, 9, 12));
+            CreateFigure(listOfFigures, new EquilateralTriangle(6, 6, 6));
+            CreateFigure(listOfFigures, new IsoscelesTriangle(5, 6, 6));
+            CreateFigure(listOfFigures, new RightTriangle(3, 4, 5));
+            CreateFigure(listOfFigures,new Circle(5));
+            
             DisplayFigures(listOfFigures);
 
             Console.WriteLine("Колличество фигур: " + listOfFigures.Count);
@@ -93,6 +31,29 @@ namespace OOPLessons
 
         }
 
+        public static void CreateFigure(List<Figure> listOfFigures, Figure figure)
+        {
+            try
+            {
+                listOfFigures.Add(figure);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void CreateFigure(List<IPerimeterCalculation> figuresCountingPerimeter, IPerimeterCalculation figure)
+        {
+            try
+            {
+                figuresCountingPerimeter.Add(figure);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
         public static void DisplayFigures(List<Figure> figuresCollection)
         {
             if (figuresCollection == null)
