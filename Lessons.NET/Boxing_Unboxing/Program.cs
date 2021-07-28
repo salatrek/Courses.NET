20655ae0328e19950a09002b635a7bf82bf23c50
aam <alexei270396@gmail.com>
2021-07-28 10:05:36 +0300

    Update Boxing, Unboxing method. Variable initialization moved outside the loop.

--- Lessons.NET/Boxing_Unboxing/Program.cs
+++ Lessons.NET/Boxing_Unboxing/Program.cs
@@ -11,40 +11,52 @@ namespace Boxing_Unboxing
     {
         static void Main(string[] args)
         {
-            var timeSpent = Boxing();
+            double boxingTime = 0;
+            double unboxingTime = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                boxingTime += Boxing();
 
-            Unboxing();
+                unboxingTime += Unboxing();
+            }
 
+            Console.WriteLine($"Время упаковки: {boxingTime}    Время распаковки: {unboxingTime}");
+            
             Console.ReadLine();
         }
         private static double Boxing()
         {
             Stopwatch stopwatch = new Stopwatch();
+
+            int a = 25;
             stopwatch.Start();
             for (int i = 0; i < 1000000; i++)
             {
-                int a = 25;
                 object b = a;
             }
             stopwatch.Stop();
             
             var time_spent = stopwatch.Elapsed.TotalMilliseconds;
-            Console.WriteLine("Время упаковки: " + time_spent);
+            //Console.WriteLine("Время упаковки: " + time_spent);
             return time_spent;
+            
         }
-        private static void Unboxing()
+        private static double Unboxing()
         {
             Stopwatch stopwatch = new Stopwatch();
+
+            object c = 25;
             stopwatch.Start();
             for (int i = 0; i < 1000000; i++)
             {
-                object b = 5;
-                int c = (int)b;
+                int d = (int)c;
             }
             stopwatch.Stop();
 
-            var UnBoxing = stopwatch.Elapsed.TotalMilliseconds;
-            Console.WriteLine("Время распаковки: " + UnBoxing);
+            var time_spent = stopwatch.Elapsed.TotalMilliseconds;
+            //Console.WriteLine("Время распаковки: " + time_spent);
+
+            return time_spent;
         }
 
 
