1eabcdf4fb959197d8d28def3ae08e182e2855ef
salatrek <alexei270396@gmail.com>
2021-08-30 18:11:59 +0300

    Thread

---
+++ Lessons.NET/TwelfthLesson(Threading)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace TwelfthLesson_Threading_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Thread(PrintX);
+            t.Start();
+            
+            
+            while (true)
+            {
+                Console.WriteLine("Y");
+                Thread.Sleep(500);
+            }
+        }
+
+        private static void PrintX()
+        {
+            while (true)
+            {
+                Console.WriteLine("X");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
