271c97480216e119a1ba57014bd7acb488f9d1dc
AmberWarder <alexei.270396@gmail.com>
2021-09-04 23:27:03 +0300

    Regular

---
+++ Lessons.NET/RegularExpression/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegularExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = "1, 1234, 6000000, 135.25 , 55, 54, 156, 456789, 4";
+            string pattern = @"\d{1}";
+            string pattern2 = @"\d{4}";
+            string pattern3 = @"\d{7}";
+            string pattern4 = @"(\d*\.\d{2}?){1}";
+
+            Console.WriteLine($"{Regex.Match(numbers, pattern2)}");
+
+            string url = "https://ya.ru/api?r=1&x=23";
+            string urlPattern = @".*\?(((?<param>\w*)=(?<value>\w*))&?)*";
+            Console.WriteLine(Regex.IsMatch(url, urlPattern));
+
+            var urlParams = Regex.Matches(url, urlPattern);
+            foreach(Match match in urlParams)
+            {
+                var paramGroup = match.Groups["param"];
+                var valueGroup = match.Groups["value"];
+
+                for (int i =0; i < paramGroup.Captures.Count; i++)
+                {
+                    Console.WriteLine($"param name - {paramGroup.Captures[i]} param value - {valueGroup.Captures[i]}");
+                }
+            }
+
+            var numberPattern = @"^((\+373|0)\s?)?((77[4-9]\s?)|(\(77[4-9]\)\s?))\d{5}$";
+
+            Console.WriteLine(Regex.IsMatch("77882344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("077882344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("+37377882344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("0 77882344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("778 82344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("0778 82344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("0 778 82344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("0(778)82344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("0 (778)82344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("0(778) 82344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("0 (778) 82344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("+373(778)82344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("+373 (778)82344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("+373 (778) 82344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("+373 77882344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("+373 778 82344", numberPattern));
+
+            Console.WriteLine(Regex.IsMatch("+373 778 882344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("+373 (778 82344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("373 778 82344", numberPattern));
+            Console.WriteLine(Regex.IsMatch("778  82344", numberPattern));
+
+            string text = "Учебный текст  с дублирующимися пробелами  для выполнения  задания.";
+            string replacement = " ";
+            string textPattern = @"\s{2,}";
+            Console.WriteLine(Regex.Replace(text, textPattern, replacement));
+
+
+        }
+    }
+}
--- Lessons.NET/TwelfthLesson(Threading)/Program.cs
+++ Lessons.NET/TwelfthLesson(Threading)/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace TwelfthLesson_Threading_
@@ -7,24 +8,44 @@ namespace TwelfthLesson_Threading_
     {
         static void Main(string[] args)
         {
-            var t = new Thread(PrintX);
-            t.Start();
-            
-            
-            while (true)
+            var dict = new Dictionary<string, string>();
+            var locker = new object();
+            var count = 10000;
+            var completed = 0;
+
+            ThreadPool.QueueUserWorkItem(_ => // запуск параллельного потока
             {
-                Console.WriteLine("Y");
-                Thread.Sleep(500);
-            }
+                var c = count;
+                while (c-- > 0)
+                {
+                    lock (locker) // синхронизация
+                    {
+                        dict.Add("1_" + c, "hello thread1");
+                    }
+                }
+
+                Interlocked.Increment(ref completed); // потокобезопасный инкремент числа
+            });
+
+            ThreadPool.QueueUserWorkItem(_ => // запуск параллельного потока
+            {
+                var c = count;
+                while (c-- > 0)
+                {
+                    lock (locker) // синхронизация
+                    {
+                        dict.Add("2_" + c, "hello thread2");
+                    }
+                }
+
+                Interlocked.Increment(ref completed); // потокобезопасный инкремент числа
+            });
+
+            //while (completed < 2) // ожидание завершения
+            //{
+            //    Thread.Sleep(25);
+            //}
         }
 
-        private static void PrintX()
-        {
-            while (true)
-            {
-                Console.WriteLine("X");
-                Thread.Sleep(500);
-            }
-        }
     }
 }
